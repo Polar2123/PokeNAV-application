0e393cf5822b261b19724cda14f2028cd72ff3f7
Sivert Bronebakk <sivertbronebakk@gmail.com>
2024-09-20 15:43:29 +0200

    Updates

--- zodiac_signs.py
+++ zodiac_signs.py
@@ -3,23 +3,24 @@ def get_zodiac_signs():
     if birth_month < 1 or birth_month > 12:
         print("Error - The month index provided is not valid. Choose between 1 and 12.")
     else:
-        zodiac_list = [
-            ["Earth","Capricorn","Leafeon"],
-            ["Air","Aquarius","Jolteon"],
-            ["Water","Pisces","Vaporeon"],
-            ["Fire","Aries","Flareon"],
-            ["Earth","Taurus","Leafeon"],
-            ["Air","Gemini","Jolteon"],
-            ["Water","Cancer","Vaporeon"],
-            ["Fire","Leo","Flareon"],
-            ["Earth","Virgo","Leafeon"],
-            ["Air","Libra","Jolteon"],
-            ["Water","Scorpio","Vaporeon"],
-            ["Fire","Sagittarius","Flareon"]
-        ]
-        user_element = zodiac_list[birth_month - 1][0]
-        zodiac_sign = zodiac_list[birth_month-1][1]
-        user_eeveelution = zodiac_list[birth_month-1][2]
+        zodiac_traits = (
+            ("Earth","Capricorn","Leafeon"),
+            ("Air","Aquarius","Jolteon"),
+            ("Water","Pisces","Vaporeon"),
+            ("Fire","Aries","Flareon"),
+            ("Earth","Taurus","Leafeon"),
+            ("Air","Gemini","Jolteon"),
+            ("Water","Cancer","Vaporeon"),
+            ("Fire","Leo","Flareon"),
+            ("Earth","Virgo","Leafeon"),
+            ("Air","Libra","Jolteon"),
+            ("Water","Scorpio","Vaporeon"),
+            ("Fire","Sagittarius","Flareon")
+        )
+        zodiac_index = zodiac_traits[birth_month-1]
+        user_element = zodiac_index[0]
+        zodiac_sign = zodiac_index[1]
+        user_eeveelution = zodiac_index[2]
         print(f"Zodiac sign: {zodiac_sign}\nElement: {user_element}\nEeveelution: {user_eeveelution}")
 
-zodiac_signs()
+get_zodiac_signs()

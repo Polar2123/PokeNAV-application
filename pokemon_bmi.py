9ff3f9965503db4b78162309b51196dc5435163d
Polar2123 <107853918+Polar2123@users.noreply.github.com>
2024-09-20 16:32:47 +0200

    Update pokemon_bmi.py

--- pokemon_bmi.py
+++ pokemon_bmi.py
@@ -2,9 +2,8 @@ def find_pokemon_bmi():
     pokemon_height = float(input("Type the pokemon height: "))
     pokemon_weight = float(input("Type the pokemon weight: "))
 
-    if pokemon_height == 0:
-        print("Error - Height cannot be 0.")
-    elif pokemon_height < 0 and pokemon_weight < 0:
+
+    if pokemon_height < 0 and pokemon_weight < 0:
         print("Error - Height and weight must be a positive numbers.")
     elif pokemon_height < 0:
         print("Error - Height must be a positive number.")
@@ -20,4 +19,3 @@ def find_pokemon_bmi():
             print(f"BMI = {pokemon_bmi:.2f}. The pokemon is overweight.")
         elif pokemon_bmi >= 85:
             print(f"BMI = {pokemon_bmi:.2f}. The pokemon is obese.")
-find_pokemon_bmi()

3d8e4596c9752c20a593b1ee09fab177968b3e31
Dayush82 <tsetsenbat82@gmail.com>
2024-09-24 18:06:38 +0200

    changes

--- get_hashtags.py
+++ get_hashtags.py
@@ -4,13 +4,11 @@ def get_hashtags():
     #user input
     text = input("Type your post: ")
     #check user input contains #
-    if "#" in text:
+    if "#" in text[0]:
         list_text = text.split(" ") #split to list
-        for i in range(len(list_text)):
-            if "#" in list_text[i][0]: 
-                hashtag_word = list_text[i]
-                if hashtag_word not in hastags_list:
-                    hastags_list.append(hashtag_word)
+        hashtag_word = list_text[i]
+        if hashtag_word not in hastags_list:
+            hastags_list.append(hashtag_word)
         print("Hashtags found:")
         for i in range(len(hastags_list)):
                 print(hastags_list[i])
--- health_tracker.py
+++ health_tracker.py
@@ -7,37 +7,41 @@ def health_tracker():
     max_active_day=""
     least_active_day =""
     weekday=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    list_set_steps =[]
 
     set_day = input("Steps Statistics: ")
-    if set_day == "": #if set is empty
-        list_set_day= [] #list will be empty
+    if set_day == "" or set_day == " ": #if set is empty
+        list_set_steps= [] #list will be empty
     else:
-        list_set_day = set_day.split(",") #else will split into list
-    length_of_list_day = len(list_set_day)
+        list_set_str_day = set_day.split(",") #else will split into list
+        for steps in list_set_str_day:
+            list_set_steps.append(int(steps))        
+        
+    length_of_list_day = len(list_set_steps)
 
     #Check if the user provided exact 7 days
     if length_of_list_day == 7:
 
         #Calculate the mean(average) steps
-        for i in range(len(list_set_day)):
-            sum_of_steps +=int(list_set_day[i])
+        for i in range(len(list_set_steps)):
+            sum_of_steps +=list_set_steps[i]
 
-        mean = sum_of_steps / len(list_set_day)
+        mean = sum_of_steps / len(list_set_steps)
         #Calculate the standart deviation 
-        for i in range(len(list_set_day)):
-            sum_of_steps_means +=(int(list_set_day[i]) - mean)**2
-        std_steps = (sum_of_steps_means / len(list_set_day))**0.5
+        for steps in list_set_steps:
+            sum_of_steps_means +=(steps - mean)**2
+        std_steps = (sum_of_steps_means / len(list_set_steps))**0.5
         #Calculate the max and least active day
         for i in range(length_of_list_day):
             if i == 0:
-                max_steps = int(list_set_day[i])
-                least_steps = int(list_set_day[i])
+                max_steps = list_set_steps[i]
+                least_steps = list_set_steps[i]
 
-            if max_steps <= int(list_set_day[i]):
-                max_steps = int(list_set_day[i])
+            if max_steps <= list_set_steps[i]:
+                max_steps = list_set_steps[i]
                 max_active_day = weekday[i]
-            if least_steps >= int(list_set_day[i]):
-                least_steps = int(list_set_day[i])
+            if least_steps >= list_set_steps[i]:
+                least_steps = list_set_steps[i]
                 least_active_day = weekday[i]
         #OUTPUT
         message = f"""{mean:.2f} + / - {std_steps:.2f} per day.
